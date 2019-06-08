653ef16292cf272917d86a355b9747198da19e83
Pomalini <42066228+Pomalini@users.noreply.github.com>
2019-06-08 10:07:56 +0530

    Update and rename Rev.java to  RevWithoutSpclChar.java

--- RevWithoutSpclChar.java
+++ RevWithoutSpclChar.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Rev {
+public class RevWithoutSpclChar {
 	public static void main(String args[]){
 	Scanner scan=new Scanner(System.in);
 	String str=scan.next();
