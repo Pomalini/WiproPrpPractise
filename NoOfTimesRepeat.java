4ce96bb982ce2e8cb976693473fd053ae8d78ace
Pomalini <42066228+Pomalini@users.noreply.github.com>
2019-06-10 12:15:08 +0530

    Update NoOfTimesRepeat.java

--- NoOfTimesRepeat.java
+++ NoOfTimesRepeat.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
-public class PrintNoOfTimes {
+public class NoOfTimesRepeat {
 	public static void main(String args[]){
 		Scanner scan=new Scanner(System.in);
 		String str=scan.next();
 		String rep="";
 		String p="";
 		int j=0;
-		for(int i=0;i<str.length()-1;i++){
+		for(int i=0;i<str.length();i++){
 			p="";
-			if(str.charAt(i)>='0' &&str.charAt(i)<='9'){
+			if(i==str.length()-1){
+				p+=str.charAt(i);
+				int val=Integer.parseInt(p);
+				
+				for(int k=0;k<val;k++){
+					rep=rep+str.substring(j,j+1);
+				
+				}
+				
+			}
+			if(str.charAt(i)>='0' &&str.charAt(i)<='9' &&i!=str.length()-1){
+				
 				if(Character.isDigit(str.charAt(i+1))){
 				p+=str.substring(i,i+2);
 			}
@@ -21,6 +32,7 @@ public class PrintNoOfTimes {
 				j=i;
 				continue;
 			}
+		
 			int val=Integer.parseInt(p);
 			
 			for(int k=0;k<val;k++){
