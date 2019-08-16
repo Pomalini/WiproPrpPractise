c2dcbf8689d132abcadae9581976f23f3ba6305a
Pomalini <42066228+Pomalini@users.noreply.github.com>
2019-08-16 14:46:05 +0530

    Create CamelCase.java

---
+++ CamelCase.java
@@ -0,0 +1,27 @@
+package logic;
+
+import java.util.Scanner;
+
+public class CamelCase {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String str=scan.nextLine();
+		String a[]=str.split(" ");
+		String s="",res="";
+		for(int i=0;i<a.length;i++){
+			s=a[i].toLowerCase();
+			for(int j=0;j<s.length();j++){
+				if(j==0){
+				res+=s.substring(j,j+1).toUpperCase();
+				}
+				else{
+				res+=s.substring(j,j+1);
+			}
+			}
+			res+=" ";
+			
+		}
+		System.out.println(res);
+	}
+
+}
