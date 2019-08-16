e8cb8aff536d71dcc340c7ca5d709c59ab7d95e2
Pomalini <42066228+Pomalini@users.noreply.github.com>
2019-08-16 12:29:31 +0530

    Create ProductArrWithoutItsIndex.java

---
+++ ProductArrWithoutItsIndex.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ProductArrWithoutItsIndex {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=scan.nextInt();
+		}
+		int pro=1;
+		for(int i=0;i<n;i++){
+			pro=1;
+			for(int j=0;j<n;j++){
+				if(i!=j){
+					pro=pro*arr[j];
+				}
+			}
+			System.out.print(pro+" ");
+		}
+		
+	}
+}
+
+
